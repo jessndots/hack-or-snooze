578308d5f25f2034695b64779f93bef24b35599e
Jessica Doty <jessndoty@gmail.com>
2021-10-06 12:53:19 -0700

    fix functions for when user is not logged in

--- js/stories.js
+++ js/stories.js
@@ -7,6 +7,7 @@ let storyList;
 /** Get and show stories when site first loads. */
 
 async function getAndShowStoriesOnStart() {
+  console.debug('getAndShowStoriesOnStart')
   storyList = await StoryList.getStories();
   $storiesLoadingMsg.remove();
 
@@ -25,11 +26,13 @@ function generateStoryMarkup(story) {
   // console.debug("generateStoryMarkup", story);
 
   const hostName = story.getHostName();
-  const favoritesArray = currentUser.favorites
 
-  for (let i = 0; i < favoritesArray.length; i++) {
-    if (favoritesArray[i].storyId == story.storyId) {
-      return $(`
+  if (currentUser) {
+    const favoritesArray = currentUser.favorites
+
+    for (let i = 0; i < favoritesArray.length; i++) {
+      if (favoritesArray[i].storyId == story.storyId) {
+        return $(`
         <li id="${story.storyId}">
         <a href="${story.url}" target="a_blank" class="story-link">
         ${story.title}
@@ -40,6 +43,7 @@ function generateStoryMarkup(story) {
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" /><i class="fa fa-heart heart-filled" aria-hidden="true">
         </li>
       `);
+      }
     }
   }
   return $(`
@@ -81,12 +85,17 @@ function putFavoritesOnPage() {
   $favoriteStoriesList.empty();
 
   // loop through all of favorite stories and generate HTML for them
-  for (let story of currentUser.favorites) {
-    const $story = generateStoryMarkup(story);
-    $favoriteStoriesList.append($story);
+  if (currentUser) {
+    for (let story of currentUser.favorites) {
+      const $story = generateStoryMarkup(story);
+      $favoriteStoriesList.append($story);
+    }
+
+    $favoriteStoriesList.show();
+  } else {
+    $favoriteStoriesTitle.text('Please login or create an account to add stories to your favorites.')
   }
 
-  $favoriteStoriesList.show();
 }
 
 
@@ -96,19 +105,23 @@ async function submitStory(evt) {
   console.debug("submit story", evt);
   evt.preventDefault();
 
-  // grab the title, author, and url
-  const title = $("#story-title").val();
-  const author = $("#story-author").val();
-  const url = $("#story-url").val();
+  if (currentUser) {
+    // grab the title, author, and url
+    const title = $("#story-title").val();
+    const author = $("#story-author").val();
+    const url = $("#story-url").val();
 
-  // ** Adds story data to API, makes a Story instance, adds it to story list.
-  // - user - the current instance of User who will post the story
-  // - obj of {title, author, url}
-  // Returns the new Story instance
-  const newStory = await storyList.addStory(currentUser, { title, author, url });
+    // ** Adds story data to API, makes a Story instance, adds it to story list.
+    // - user - the current instance of User who will post the story
+    // - obj of {title, author, url}
+    // Returns the new Story instance
+    const newStory = await storyList.addStory(currentUser, { title, author, url });
 
-  $storyForm.trigger("reset");
+    $storyForm.trigger("reset");
+  } else {
+    $storyForm.append('<p class="red">You must login or create an account in order to submit a story.</p>')
 
+  }
 }
 
 $storyForm.on("submit", submitStory);
@@ -116,14 +129,15 @@ $storyForm.on("submit", submitStory);
 $body.on("click", "i", async function (evt) {
   const targetId = $(evt.target).parent().attr("id"); //id of story we want to favorite or unfavorite
   const targetStory = (await axios.get(`${BASE_URL}/stories/${targetId}`)).data.story //story with that id
+  if (currentUser) {
+    $(evt.target).toggleClass("fa-heart-o heart-outline fa-heart heart-filled"); //change the heart icon
 
-  $(evt.target).toggleClass("fa-heart-o heart-outline fa-heart heart-filled"); //change the heart icon
-
-  for (let i = 0; i < currentUser.favorites.length; i++) { //loop through favorites list
-    if (currentUser.favorites[i].storyId == targetId) { //if we find one that matches the target id
-      currentUser.removeFromFavorites(currentUser.favorites[i]); // then remove that story from favorites
-      return; //and exit function
+    for (let i = 0; i < currentUser.favorites.length; i++) { //loop through favorites list
+      if (currentUser.favorites[i].storyId == targetId) { //if we find one that matches the target id
+        currentUser.removeFromFavorites(currentUser.favorites[i]); // then remove that story from favorites
+        return; //and exit function
+      }
     }
+    currentUser.addToFavorites(targetStory); // if story is not in favorites list, then add to favorites
   }
-  currentUser.addToFavorites(targetStory); // if story is not in favorites list, then add to favorites
 })
